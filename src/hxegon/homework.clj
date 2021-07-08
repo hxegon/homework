408ff48ee8fb913e32c3b61284e383551dd00341
Cooper LeBrun <cooperlebrun@gmail.com>
2021-07-08 15:57:38 -0700

    Step 1 Cli option parsing

--- src/hxegon/homework.clj
+++ src/hxegon/homework.clj
@@ -1,12 +1,32 @@
 (ns hxegon.homework
+  (:require [clojure.string :as string]
+            [clojure.tools.cli :refer [parse-opts]]
+            [clojure.java.io :as io])
   (:gen-class))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+; NOTE: For performance and extensibility reasons this may make more sense as
+; a protocol.
+(def people-sorters
+  {:lastname #()
+   :birthdate #()
+   :gender #()})
+
+(def cli-options
+  [["-f" "--file FILE" "Input file"
+    :multi true ; Can specify multiple files
+    :default []
+    :update-fn conj ; join values with default into list of files
+    :validate [#(.exists (io/file %)) "Input file doesn't exist"]]
+
+   ["-s" "--sort=method" "Sorting method. Either lastname, birthdate, or gender (defaults to lastname)."
+    :default :lastname
+    :parse-fn #(->> % string/lower-case keyword)
+    :validate [#(#{(keys people-sorters)} %) ; input to validate fn is output of parse-fn (a lower case keyword)
+               "Sorting option must be either lastname, birthdate or gender"]] ; TODO: DRY by generate options from people-sorters
+   ["-h" "--help"]])
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "For now just prints the parsed option map"
   [& args]
-  (greet {:name (first args)}))
+  (print
+    (parse-opts args cli-options)))
--- test/hxegon/homework_test.clj
+++ test/hxegon/homework_test.clj
@@ -1,7 +1,20 @@
 (ns hxegon.homework-test
   (:require [clojure.test :refer :all]
-            [hxegon.homework :refer :all]))
+            [hxegon.homework :as hw]
+            [clojure.string :as str]
+            [clojure.tools.cli :refer [parse-opts]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest cli-options-test
+  (testing "-f rejects files that don't exist with error"
+    (let [args ["-f" "Doesntexist.txt"]
+          parsed (parse-opts args hw/cli-options)]
+      (is (empty? (get-in parsed [:options :file])))
+      (is (->> parsed :errors empty? not))))
+  (testing "-f can handle multiple files"
+    (let [args ["-f" "deps.edn" "-f" "README.md"]
+          parsed (parse-opts args hw/cli-options)]
+      (is (= 2 (count (get-in parsed [:options :file]))))))
+  (testing "-s errors with invalid method name"
+    (let [args ["-s" "foo"]
+          parsed (parse-opts args hw/cli-options)]
+      (is (->> parsed :errors empty? not)))))
