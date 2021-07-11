8635b1f9b4048557ae2c912e223b855fa6208b40
Cooper LeBrun <cooperlebrun@gmail.com>
2021-07-10 17:54:04 -0700

    change person to return :error-msg string instead of :errors []

--- src/hxegon/person.clj
+++ src/hxegon/person.clj
@@ -14,10 +14,10 @@
   "Constructor for Person with validation. Returns either Person or map with :errors"
   [args]
   (if (not= 5 (count args))
-    {:errors [(str "Expected 5 arguments but got " (count args))]}
+    {:error-msg (str "Expected 5 arguments but got " (count args))}
     (let [[lastname firstname gender fav-color dob-string] args
           date-format (java.text.SimpleDateFormat. "MM/dd/yyyy")
           dob (safe-parse date-format dob-string)]
       (if (:error-msg dob)
-        {:errors [(:error-msg dob)]}
+        dob
         (->Person lastname firstname gender fav-color (:token dob))))))
--- test/hxegon/person_test.clj
+++ test/hxegon/person_test.clj
@@ -6,15 +6,15 @@
 (def valid-person-args ["last-name" "first-name" "male" "blue" "01/01/2000"])
 
 (deftest person-test
-  (testing "with incorrect number of fields returns errors"
+  (testing "with incorrect number of fields returns error"
     (let [p (p/person (conj valid-person-args "extra arg"))]
-      (is (->> p :errors empty? not)))
+      (is (->> p :error-msg nil? not)))
     (let [p (p/person (rest valid-person-args))]
-      (is (->> p :errors empty? not))))
-  (testing "with invalid date returns errors"
+      (is (->> p :error-msg nil? not))))
+  (testing "with invalid date returns error"
     (let [args (-> valid-person-args drop-last (conj "0101-2020"))
           p (p/person args)]
-      (is (->> p :errors empty? not))))
+      (is (->> p :error-msg nil? not))))
   (testing "with valid arguments returns something with person keys"
     (let [p (p/person valid-person-args)]
       (is (every? #(contains? p %) [:firstname :lastname :gender :fav-color :dob])))))
